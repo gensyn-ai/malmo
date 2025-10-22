73b4f2e57b9e2f787da1cb7ec04a58369eea0a15
Eamonn Nugent <4699217+space55@users.noreply.github.com>
2025-10-22 11:46:36 -0400

    Changes to support new skin

--- scripts/python-wheel/package/setup.py
+++ scripts/python-wheel/package/setup.py
@@ -115,7 +115,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='mbag-malmo',  # Required
+    name='mbag-malmo-gensyn',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
